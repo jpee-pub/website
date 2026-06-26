b0d75d65b41d1e3eeae6e927853067ccdc2b4b16
YukiKaneko211 <illuminate.kmkz@outlook.com>
2026-06-26 04:57:09 +0300

    最新情報セクション追加

--- src/components/1. Intro.tsx
+++ src/components/1. Intro.tsx
@@ -1,7 +1,7 @@
 export function Intro() {
   return (
     <section id="intro" className="relative flex flex-col items-center justify-center overflow-hidden">
-      <div className="mt-30 mb-10 text-[#0072ce] flex flex-col items-center">
+      <div className="mt-30 mb-10 mr-5 ml-5 text-[#0072ce] flex flex-col items-center">
         <p className="w-full text-center text-2xl font-bold">
           私たちJPEE+（じぇぺーぷらす）は欧州と日本を「近く」します！
         </p>
